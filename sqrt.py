8a0c49822305e7567ec6a11675e7cae459954b89
Liam <31958679+liam-pk@users.noreply.github.com>
2018-01-26 21:12:53 -0500

    Delete sqrt.py

--- sqrt.py
+++
@@ -1,9 +0,0 @@
-def sqrt(n):
-    approx = n/2
-    while True:
-        better = (approx + n/approx)/2
-        if abs(better - approx) < .001:
-            return better
-        approx
-        
-print("sqrt",sqrt(25.0))
