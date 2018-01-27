ec8648d42682d3496fbac636e1a9faffa1628110
Liam <31958679+liam-pk@users.noreply.github.com>
2018-01-26 21:12:42 -0500

    Delete Homework7.5 2.py

--- Homework7.5 2.py
+++
@@ -1,8 +0,0 @@
-def sumexclude(data):
-    "Sum all the elements in a list up to but not including the first even number."
-    count = 0
-    for i in data:
-        if i % 2 == 0:
-            break
-        count += i
-    return count
